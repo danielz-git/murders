c9b208f91a4a03a23606159dd5ed6d08d9ffbec6
Daniel Zamorano <daniel.a.zamorano@outlook.com>
2025-01-10 17:50:40 -0600

    Adding all the files at once.

---
+++ download-data.R
@@ -0,0 +1,4 @@
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dest_file <- "C:/Users/Grape/Documents/GTx OMSA/Prep/HardvardX PH125/homework-0/data/murders.csv"
+download.file(url, destfile=dest_file)
+# did not get to work...
